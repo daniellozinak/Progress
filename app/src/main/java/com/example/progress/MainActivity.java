2511ad98b7c6b5dfd81b74bae7a24829c61b2bae
daniellozinak <danolozinak@gmail.com>
2020-10-24 23:04:51 +0200

    Added Basic UI Layout

--- app/src/main/java/com/example/progress/MainActivity.java
+++ app/src/main/java/com/example/progress/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.progress;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startWorkoutActivity(View view) {
+        Intent mIntent = new Intent(this,WorkoutActivity.class);
+        startActivity(mIntent);
+    }
+
+    public void startProfileActivity(View view) {
+        Intent mIntent = new Intent(this,ProfileActivity.class);
+        startActivity(mIntent);
+    }
+
+    public void startHistoryActivity(View view) {
+        Intent mIntent = new Intent(this,HistoryActivity.class);
+        startActivity(mIntent);
     }
 }
